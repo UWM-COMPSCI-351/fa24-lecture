004d3006fad61a6e40b95bb99e9445166cb2f0c0
John Boyland <boyland@uwm.edu>
2024-08-22 16:16:19 -0500

    Messed up indentation

--- src/edu/uwm/cs351/Student.java
+++ src/edu/uwm/cs351/Student.java
@@ -6,13 +6,11 @@ public class Student {
 	
 	public static Student method(Student[] a) {
 		double tmp = 0.0;
-		for (int i=0; i < 10; ++i) {
-			if (a[i].score > tmp) tmp = a[i].score;
-		}
-		for (int i=0; i < 10; ++i) {
-			if (a[i].score == tmp) return a[i];
-		}
-		System.out.println("No one is best");
+		for (int i=0; i < 10; i++)
+			if (a[i].score > tmp) 
+				tmp = a[i].score;
+			for (int i=0; i < 10; i++) if (a[i].score == tmp) return a[i];
+System.out.println("No one is best");
 		return null;
 	}
 }
