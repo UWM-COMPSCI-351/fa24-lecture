d0e5fc4cce6d5492f89f4209e48625579155e99c
John Boyland <boyland@Uwm.edu>
2024-10-01 15:25:57 -0500

    Initial insertion sort pieces.

---
+++ src/edu/uwm/cs351/InsertionSort.java
@@ -0,0 +1,16 @@
+package edu.uwm.cs351;
+
+import java.util.Comparator;
+
+public class InsertionSort<E> {
+
+	private final Comparator<E> comp;
+	
+	public InsertionSort(Comparator<E> c) {
+		comp = c;
+	}
+	
+	public void sort(E[] array) {
+		
+	}
+}
